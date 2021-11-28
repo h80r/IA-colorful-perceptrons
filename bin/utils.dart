f2fb93baba36f5b8bd9c572f1a033efaefdebfbe
h80r <h80rgbc@gmail.com>
2021-11-28 01:00:57 -0300

    Update isEquals functions

--- bin/utils.dart
+++ bin/utils.dart
@@ -1,17 +1,27 @@
 import 'dart:math';
 
-extension Zipper on List {
-  List<List<T>> zip<T>(List<T> other) {
+extension Zipper on List<double> {
+  List<List<double>> zip(List<double> other) {
     return List.generate(
       min(length, other.length),
       (index) => [this[index], other[index]],
     );
   }
 
-  bool isEquals(List other) {
+  bool isEquals(List<double> other) {
     if (length != other.length) return false;
     for (var i = 0; i < length; i++) {
       if (this[i] != other[i]) return false;
+    }
+    return true;
+  }
+}
+
+extension Equalizer on List<List<double>> {
+  bool isEquals(List<List<double>> other) {
+    if (length != other.length) return false;
+    for (var i = 0; i < length; i++) {
+      if (!this[i].isEquals(other[i])) return false;
     }
     return true;
   }
